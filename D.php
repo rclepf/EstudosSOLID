52d9934560c2cb3357ea36df008c70df66a5de23
Rodolfo Clepf <rclepf@gmail.com>
2023-02-07 14:56:13 -0300

    D

---
+++ D.php
@@ -0,0 +1,41 @@
+<?php
+interface DBConnection
+{
+    public function connect();
+}
+class MySQLConnection implements DBConnection
+{
+    public function connect()
+    {
+    }
+}
+
+class OracleConnection implements DBConnection
+{
+    public function connect()
+    {
+    }
+}
+
+class MongoConnection implements DBConnection
+{
+    public function connect()
+    {
+    }
+}
+class UsuarioDAO
+{
+    private $db;
+    public function __construct(DBConnection $dbCon)
+    {
+        $this->db = $dbCon;
+    }
+}
+
+
+$dbCon = new MySQLConnection(...);
+$dbCon = new OracleConnection(...);
+$dbCon = new MongoConnection(...);
+
+$usuarioDao = new UsuarioDAO($dbCon);
+$usuario2Dao = new UsuarioDAO($dbCon);
